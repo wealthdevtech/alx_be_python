1edebc70d14078ae81de41406ccf6d552e9ece5a
wealthdevtech <wealthydaniel56@gmail.com>
2025-07-10 13:45:05 +0200

    Committing finance_calculator.py file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses = input("Enter your total monthly expenses: ")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = int(monthly_income) -  int(monthly_expenses)
-projected_annual_savings = int((int(monthly_savings) * 12) + (int(monthly_savings) * 12 * 0.05))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are: $" + str(monthly_savings) + ".")
-print("Projected savings after one year, with interest, is: $" + str(projected_annual_savings) + ".")
+print("Projected savings after one year, with interest, is: $" + str(int(projected_savings)) + ".")
