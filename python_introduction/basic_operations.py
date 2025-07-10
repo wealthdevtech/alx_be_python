2ef6c7c04339dcb02131b59469af9f0fcd8aa4a0
wealthdevtech <wealthydaniel56@gmail.com>
2025-07-10 12:36:03 +0200

    Commit of basic_operations.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+strnumber1 = str(number1)
+strnumber2 = str(number2)
+
+print("Addition of " + strnumber1 + " and " + strnumber2 + " is " + str(number1 + number2))
+print("Subtraction of " + strnumber1 + " and " + strnumber2 + " is " + str(number1 - number2))
+print("Multiplication of " + strnumber1 + " and " + strnumber2 + " is " + str(number1 * number2))
+
