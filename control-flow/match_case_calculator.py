ab67663e83ec505ada38ffe6e5f753ccdffdbe31
wealthdevtech <wealthydaniel56@gmail.com>
2025-07-17 17:47:39 +0200

    Changed variable operations to operation

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,8 +1,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operations = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operations:
+match operation:
     case "+":
         print(f"The result is {num1 + num2}")
     case "-":
