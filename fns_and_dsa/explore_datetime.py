e97dfb33bdbd0ac89a96ca5480e3ca1e2df6d6a1
wealthdevtech <wealthydaniel56@gmail.com>
2025-07-26 13:56:39 +0200

    First commit of explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date_raw = datetime.now()
+    current_date = current_date_raw.strftime("%Y-%m-%d %H:%M:%S")
+    print(current_date)
+    
+    def calculate_future_date():
+        user_input = int(input("Enter the number of days to add to the current date: "))
+        future_date_raw = timedelta(days=user_input)
+        future_date = (current_date_raw + future_date_raw).strftime("%Y-%m-%d")
+        print(f"Future date: {future_date}")
+    
+    calculate_future_date()
+    
+def main():
+    while True:
+        display_current_datetime()
+        exit = input("Type 'exit' to break loop.")
+        if exit == "exit":
+            print("Goodbye!")
+            break
+    
+main()
