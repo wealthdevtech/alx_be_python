62b2bc27c219113b253c395d92a38e12691ecdaa
wealthdevtech <wealthydaniel56@gmail.com>
2025-07-26 11:09:39 +0200

    First commit of arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("Cannot divide by 0.")
+        return num1/num2
