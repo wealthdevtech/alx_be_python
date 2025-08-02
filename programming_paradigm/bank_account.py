889e287ddf770213634e1c1e5c57694b06af6237
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-02 17:57:21 +0200

    Replaced print wuth return in display_balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 class BankAccount:
     
     def __init__(self, account_balance=0):
-        self.account_balance = account_balance;
+        self.account_balance = account_balance
         
     def deposit(self, amount):
         self.account_balance += amount
@@ -10,7 +10,7 @@ class BankAccount:
         if amount < self.account_balance:
             return False
         elif amount >= self.account_balance: 
-            self.account_balance -= amount;
+            self.account_balance -= amount
             
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}")
+        return f"Current balance: ${self.account_balance}"
