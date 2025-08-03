b698ba4b5adcaf432d0cb3fd6e6b45ed5473842f
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-03 17:42:33 +0200

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,8 +6,8 @@ class safe_divide:
     
     def safe_divide(self): 
         try: 
-            self.numerator = float(self.numerator)
-            self.denominator = float(self.denominator)
+            self.numerator = float(numerator)
+            self.denominator = float(denominator)
             return f"The result of the division is {self.numerator / self.denominator}"
         except ValueError:
             return "Error: Please enter numeric values only."
