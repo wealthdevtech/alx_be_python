7c2d29a1592b54f1a106301f8f03d687eb7db7a7
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-03 22:50:55 +0200

    First commit of lms

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,9 @@
+class Library:
+    def __init__(self, book):
+        self.book = book
+        
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
