9def205514dea70144c29e3f094df47841ac04f0
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-03 08:19:20 +0200

    Updated bank_account.py again

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,14 +4,17 @@ class BankAccount:
         self.account_balance = account_balance
         
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount > 0:
+            self.account_balance += amount
         
     def withdraw(self, amount):
-        if amount > self.account_balance:
+        if amount < 0:
+            return False
+        elif amount > self.account_balance:
             return False
         elif amount <= self.account_balance: 
             self.account_balance -= amount
             return True
-            
+        
     def display_balance(self):
-        return (f"Current balance: ${self.account_balance}")
+        return (f"Current Balance: ${self.account_balance:.2f}")
