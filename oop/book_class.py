4d2e5277514bccedaf7eedad125e908bad708710
wealthdevtech <wealthydaniel56@gmail.com>
2025-08-10 22:31:57 +0200

    Removed (.) in book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,7 +5,7 @@ class Book:
         self.year = year
         
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}."
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
